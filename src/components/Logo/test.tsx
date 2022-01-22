50b36d0e8420889daff7a4aeb48c5d9bd704e097
Washington Campos <washingtoncampos9@gmail.com>
2022-01-22 18:36:01 -0300

    test: create colors tests

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,11 +1,19 @@
-import { render } from "@testing-library/react";
+import { screen } from "@testing-library/react";
+
+import { renderWithTheme } from "utils/tests/helpers";
 
 import { Logo } from ".";
 
 describe("<Logo />", () => {
-	it("should render correctly", () => {
-		const { container } = render(<Logo />);
+	it("should render white label by default", () => {
+		renderWithTheme(<Logo />);
 
-		expect(container.firstChild).toMatchSnapshot();
+		expect(screen.getByLabelText(/Won games/i).parentElement).toHaveStyle({ color: "#FAFAFA" });
+	});
+
+	it("should render black label if black color is passed on props", () => {
+		renderWithTheme(<Logo color="black" />);
+
+		expect(screen.getByLabelText(/Won games/i).parentElement).toHaveStyle({ color: "#030517" });
 	});
 });
