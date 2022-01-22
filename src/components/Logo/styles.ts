f1b8537a3d9d12e3512de53f30fa6e0766e854d1
Washington Campos <washingtoncampos9@gmail.com>
2022-01-22 19:30:46 -0300

    feat: hide Logo label text when prop is passed

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -12,11 +12,26 @@ const wrapperModifier = {
 		width: 20rem;
 		height: 5.9rem;
 	`,
+
+	hideLabel: () => css`
+		width: 5.8rem;
+		height: 4.5rem;
+
+		svg {
+			height: 4.5rem;
+			pointer-events: none;
+		}
+
+		.text {
+			display: none;
+		}
+	`,
 };
 
 export const Wrapper = styled.div<LogoProps>`
-	${({ theme, color, size }) => css`
+	${({ theme, color, size, hideLabel }) => css`
 		${!!size && wrapperModifier[size]};
+		${!!hideLabel && wrapperModifier.hideLabel}
 
 		color: ${theme.colors[color!]};
 	`}
