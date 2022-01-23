0358eaccc9f0116c4150a1b2fadd6ebd5e637488
Washington Campos <washingtoncampos9@gmail.com>
2022-01-23 16:06:22 -0300

    test: ensure Heading renders correctly with each prop

---
+++ src/components/Heading/test.tsx
@@ -0,0 +1,47 @@
+import { screen } from "@testing-library/react";
+
+import { renderWithTheme } from "utils/tests/helpers";
+
+import { Heading } from ".";
+
+describe("<Heading />", () => {
+	it("should render children correctly", () => {
+		renderWithTheme(<Heading>Won games</Heading>);
+
+		expect(screen.getByRole("heading", { name: /Won games/i })).toBeInTheDocument();
+	});
+
+	it("Should render white color by default", () => {
+		renderWithTheme(<Heading>Won games</Heading>);
+
+		expect(screen.getByRole("heading", { name: /Won games/i })).toHaveStyle({
+			color: "#FAFAFA",
+		});
+	});
+
+	it("Should render black color if prop is passed", () => {
+		renderWithTheme(<Heading color="black">Won games</Heading>);
+
+		expect(screen.getByRole("heading", { name: /Won games/i })).toHaveStyle({
+			color: "#030517",
+		});
+	});
+
+	it("Should render a lineLeft if prop is passed", () => {
+		renderWithTheme(<Heading lineLeft>Won games</Heading>);
+
+		expect(screen.getByRole("heading", { name: /Won games/i })).toHaveStyle({
+			borderLeft: "0.7rem solid #3CD3C1",
+		});
+	});
+
+	it("Should render a lineBottom if prop is passed", () => {
+		renderWithTheme(<Heading lineBottom>Won games</Heading>);
+
+		expect(screen.getByRole("heading", { name: /Won games/i })).toHaveStyleRule(
+			"border-bottom",
+			"0.5rem solid #F231A5",
+			{ modifier: "::after" },
+		);
+	});
+});
