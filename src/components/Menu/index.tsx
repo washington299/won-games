3794ab7bdda12a718f3bd81e28b2ac1c35b715c6
Washington Campos <washingtoncampos9@gmail.com>
2022-01-26 14:45:35 -0300

    feat: create MenuFull styles

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,6 +7,7 @@ import {
 } from "@styled-icons/material-outlined";
 
 import { Logo } from "components/Logo";
+import { Button } from "components/Button";
 
 import * as S from "./styles";
 
@@ -34,6 +35,21 @@ export const Menu = () => {
 
 			<S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
 				<CloseIcon aria-label="Close menu" onClick={() => setIsOpen(false)} />
+
+				<S.MenuNav>
+					<S.MenuLink href="#">Home</S.MenuLink>
+					<S.MenuLink href="#">Store</S.MenuLink>
+				</S.MenuNav>
+
+				<S.RegisterBox>
+					<Button fullWidth size="large">
+						Log in now
+					</Button>
+					<span>or</span>
+					<S.CreateAccount href="#" title="Sign up">
+						Sign up
+					</S.CreateAccount>
+				</S.RegisterBox>
 			</S.MenuFull>
 		</S.Wrapper>
 	);
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -36,12 +36,107 @@ export const MenuGroup = styled.div`
 	`}
 `;
 
+export const MenuNav = styled.div``;
+
+export const MenuLink = styled.a`
+	${({ theme }) => css`
+		position: relative;
+		font-size: ${theme.font.sizes.medium};
+		margin: 0.3rem ${theme.spacings.small} 0;
+		text-decoration: none;
+		text-align: center;
+
+		&:hover {
+			&::after {
+				content: "";
+				position: absolute;
+				display: block;
+				height: 0.3rem;
+				background-color: ${theme.colors.primary};
+				animation: hoverAnimation 0.2s forwards;
+			}
+
+			@keyframes hoverAnimation {
+				from {
+					width: 0;
+					left: 50%;
+				}
+				to {
+					width: 100%;
+					left: 0;
+				}
+			}
+		}
+	`}
+`;
+
 type MenuFullProps = {
 	isOpen: boolean;
 };
 
 export const MenuFull = styled.nav<MenuFullProps>`
-	${({ isOpen }) => css`
+	${({ theme, isOpen }) => css`
 		opacity: ${isOpen ? 1 : 0};
+		pointer-events: ${isOpen ? "all" : "none"};
+
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		height: 100vh;
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		right: 0;
+		left: 0;
+		overflow-y: hidden;
+		background-color: ${theme.colors.white};
+
+		> svg {
+			width: 2.4rem;
+			height: 2.4rem;
+			position: absolute;
+			top: 0;
+			right: 0;
+			margin: ${theme.spacings.xsmall};
+			cursor: pointer;
+		}
+
+		${MenuNav} {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			flex: 1;
+		}
+
+		${MenuLink} {
+			color: ${theme.colors.black};
+			font-size: ${theme.font.sizes.xlarge};
+			font-weight: ${theme.font.bold};
+			margin-bottom: ${theme.spacings.small};
+		}
+	`}
+`;
+
+export const RegisterBox = styled.div`
+	${({ theme }) => css`
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 0 ${theme.spacings.xlarge} ${theme.spacings.xlarge};
+
+		> span {
+			display: block;
+			margin: ${theme.spacings.xxsmall} 0;
+			font-size: ${theme.font.sizes.xsmall};
+		}
+	`}
+`;
+
+export const CreateAccount = styled.a`
+	${({ theme }) => css`
+		color: ${theme.colors.primary};
+		border-bottom: 0.2rem solid ${theme.colors.primary};
+		text-decoration: none;
 	`}
 `;
