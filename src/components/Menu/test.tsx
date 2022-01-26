0467e92d2403975d2d0f4828cc07e9559ee11728
Washington Campos <washingtoncampos9@gmail.com>
2022-01-26 15:07:40 -0300

    test: esnure Menu renders correct elements when user is logged in/out

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -31,4 +31,22 @@ describe("<Menu />", () => {
 		expect(menuFull.getAttribute("aria-hidden")).toBe("true");
 		expect(menuFull).toHaveStyle({ opacity: 0 });
 	});
+
+	it("should show correct elements on menu if logged out", () => {
+		renderWithTheme(<Menu />);
+
+		expect(screen.queryByText(/My account/i)).not.toBeInTheDocument();
+		expect(screen.queryByText(/Wishlist/i)).not.toBeInTheDocument();
+		expect(screen.getByText(/Log in now/i)).toBeInTheDocument();
+		expect(screen.getByText(/Sign up/i)).toBeInTheDocument();
+	});
+
+	it("should show correct elements on menu if logged in", () => {
+		renderWithTheme(<Menu username="Washington" />);
+
+		expect(screen.queryByText(/Log in now/i)).not.toBeInTheDocument();
+		expect(screen.queryByText(/Sign up/i)).not.toBeInTheDocument();
+		expect(screen.queryByText(/My account/i)).toBeInTheDocument();
+		expect(screen.queryByText(/Wishlist/i)).toBeInTheDocument();
+	});
 });
