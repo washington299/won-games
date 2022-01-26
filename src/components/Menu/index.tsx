6bc8ebfa2dbc984c58f036dcc94444657a488056
Washington Campos <washingtoncampos9@gmail.com>
2022-01-26 15:43:23 -0300

    feat: add Menu elements on bigger devices

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -8,6 +8,7 @@ import {
 
 import { Logo } from "components/Logo";
 import { Button } from "components/Button";
+import { MediaMatch } from "components/MediaMatch";
 
 import * as S from "./styles";
 
@@ -20,13 +21,22 @@ export const Menu = ({ username = "" }: MenuProps) => {
 
 	return (
 		<S.Wrapper>
-			<S.IconWrapper onClick={() => setIsOpen(true)}>
-				<MenuIcon aria-label="Open menu" />
-			</S.IconWrapper>
+			<MediaMatch lessThan="medium">
+				<S.IconWrapper onClick={() => setIsOpen(true)}>
+					<MenuIcon aria-label="Open menu" />
+				</S.IconWrapper>
+			</MediaMatch>
 
 			<S.LogoWrapper>
 				<Logo hideLabel />
 			</S.LogoWrapper>
+
+			<MediaMatch greaterThan="medium">
+				<S.MenuNav>
+					<S.MenuLink href="#">Home</S.MenuLink>
+					<S.MenuLink href="#">Store</S.MenuLink>
+				</S.MenuNav>
+			</MediaMatch>
 
 			<S.MenuGroup>
 				<S.IconWrapper>
@@ -35,6 +45,9 @@ export const Menu = ({ username = "" }: MenuProps) => {
 				<S.IconWrapper>
 					<ShoppingCartIcon aria-label="Open shopping cart" />
 				</S.IconWrapper>
+				<MediaMatch greaterThan="medium">
+					<Button size="medium">Sign in</Button>
+				</MediaMatch>
 			</S.MenuGroup>
 
 			<S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
