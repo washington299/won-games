3aab99ac5fec5c7ac967c38ac77cc720c46d16ef
Washington Campos <washingtoncampos9@gmail.com>
2022-01-28 14:59:26 -0300

    feat: create Banner component and props

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,17 @@
+import { Story, Meta } from "@storybook/react";
+
+import { Banner, BannerProps } from ".";
+
+export default {
+	title: "Banner",
+	component: Banner,
+	args: {
+		img: "https://source.unsplash.com/user/willianjusten/1042x580",
+		title: "Borderlans",
+		subtitle: "<p>Play the new <strong>Borderlands</strong> season.</p>",
+		buttonLabel: "Buy now",
+		buttonLink: "/games/borderlans",
+	},
+} as Meta;
+
+export const Default: Story<BannerProps> = args => <Banner {...args} />;
