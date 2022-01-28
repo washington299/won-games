34beaac6cc719a620fb78eb4727cf51b2eeced86
Washington Campos <washingtoncampos9@gmail.com>
2022-01-28 15:00:07 -0300

    test: ensure Banner renders with correct elements

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,31 @@
+import { screen } from "@testing-library/react";
+
+import { renderWithTheme } from "utils/tests/helpers";
+
+import { Banner } from ".";
+
+const bannerMock = {
+	img: "https://source.unsplash.com/user/willianjusten/1042x580",
+	title: "Borderlans",
+	subtitle: "<p>Play the new <strong>Borderlands</strong> season.</p>",
+	buttonLabel: "Buy now",
+	buttonLink: "/games/borderlans",
+};
+
+describe("<Banner />", () => {
+	it("should render Banner with correct elements", () => {
+		renderWithTheme(<Banner {...bannerMock} />);
+
+		expect(screen.getByRole("img", { name: /Borderlans/i })).toBeInTheDocument();
+		expect(screen.getByRole("heading", { name: /Borderlans/i })).toBeInTheDocument();
+		expect(
+			screen.getByRole("heading", {
+				name: /Play the new Borderlands season/i,
+			}),
+		).toBeInTheDocument();
+		expect(screen.getByRole("link", { name: /Buy now/i })).toHaveAttribute(
+			"href",
+			"/games/borderlans",
+		);
+	});
+});
