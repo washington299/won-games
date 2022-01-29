3bda2be2033699bbce3fe07bc8b11c5d7845b872
Washington Campos <washingtoncampos9@gmail.com>
2022-01-29 17:45:37 -0300

    test: ensure Banner renders Ribbon correctly

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -29,4 +29,16 @@ describe("<Banner />", () => {
 		);
 		expect(container.firstChild).toMatchSnapshot();
 	});
+
+	it("should render a Ribbon correctly", () => {
+		renderWithTheme(
+			<Banner {...bannerMock} ribbonText="20% off" ribbonSize="small" ribbonColor="secondary" />,
+		);
+
+		const ribbon = screen.getByText(/20% off/i);
+
+		expect(ribbon).toBeInTheDocument();
+		expect(ribbon).toHaveStyle({ backgroundColor: "#3CD3C1" });
+		expect(ribbon).toHaveStyle({ height: "2.4rem", fontSize: "1.2rem" });
+	});
 });
