c80489fe799934f8fa608ed152ce5d8a662430ec
Washington Campos <washingtoncampos9@gmail.com>
2022-02-03 16:22:35 -0300

    feat: create BannerSlider

---
+++ src/components/BannerSlider/index.tsx
@@ -0,0 +1,37 @@
+import { Slider, SliderSettings } from "components/Slider";
+import { Banner, BannerProps } from "components/Banner";
+
+import * as S from "./styles";
+
+export type BannerSliderProps = {
+	items: BannerProps[];
+};
+
+const settings: SliderSettings = {
+	dots: true,
+	arrows: false,
+	infinite: false,
+	vertical: true,
+	verticalSwiping: true,
+	responsive: [
+		{
+			breakpoint: 1170,
+			settings: {
+				vertical: false,
+				verticalSwiping: false,
+			},
+		},
+	],
+};
+
+export const BannerSlider = ({ items }: BannerSliderProps) => {
+	return (
+		<S.Wrapper>
+			<Slider settings={settings}>
+				{items.map(item => (
+					<Banner key={item.title} {...item} />
+				))}
+			</Slider>
+		</S.Wrapper>
+	);
+};
---
+++ src/components/BannerSlider/stories.tsx
@@ -0,0 +1,46 @@
+import { Story, Meta } from "@storybook/react";
+
+import { BannerSlider, BannerSliderProps } from ".";
+
+const items = [
+	{
+		img: "https://source.unsplash.com/user/willianjusten/1042x580",
+		title: "Borderlands",
+		subtitle: "<p>Play the new <strong>Borderlands</strong> season.</p>",
+		buttonLabel: "Buy now",
+		buttonLink: "/games/borderlans",
+		ribbonText: "Best seller",
+	},
+	{
+		img: "https://source.unsplash.com/user/willianjusten/1042x590",
+		title: "Borderlands 2",
+		subtitle: "<p>Play the new <strong>Borderlands 2</strong> season.</p>",
+		buttonLabel: "Buy now",
+		buttonLink: "/games/borderlans2",
+	},
+	{
+		img: "https://source.unsplash.com/user/willianjusten/1042x595",
+		title: "Borderlands 3",
+		subtitle: "<p>Play the new <strong>Borderlands 3</strong> season.</p>",
+		buttonLabel: "Buy now",
+		buttonLink: "/games/borderlans3",
+	},
+];
+
+export default {
+	title: "BannerSlider",
+	component: BannerSlider,
+	args: { items },
+	parameters: {
+		layout: "fullscreen",
+		backgrounds: {
+			default: "dark",
+		},
+	},
+} as Meta;
+
+export const Default: Story<BannerSliderProps> = args => (
+	<div style={{ maxWidth: "130rem", margin: "0 auto" }}>
+		<BannerSlider {...args} />
+	</div>
+);
