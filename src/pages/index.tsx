2da031b2e322e28358c752500260e51219db7dfc
Washington Campos <washingtoncampos9@gmail.com>
2022-02-04 17:47:27 -0300

    feat: return mock data from server to Home page and pass props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
-import { HomeTemplate } from "templates/Home";
+import { GetServerSideProps } from "next";
 
-const Index = () => {
-	return <HomeTemplate />;
+import { items as bannersMock } from "components/BannerSlider/mock";
+import { items as gamesMock } from "components/GameCardSlider/mock";
+import { item as highlightMock } from "components/Highlight/mock";
+
+import { HomeTemplate, HomeTemplateProps } from "templates/Home";
+
+const Index = (props: HomeTemplateProps) => {
+	return <HomeTemplate {...props} />;
 };
 
 export default Index;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	return {
+		props: {
+			banners: bannersMock,
+			newGames: gamesMock,
+			mostPopularHighlight: highlightMock,
+			mostPopularGames: gamesMock,
+			upcomingGames: gamesMock,
+			upcomingHighlight: highlightMock,
+			upcomingMoreGames: gamesMock,
+			freeGamesHighlight: highlightMock,
+			freeGames: gamesMock,
+		},
+	};
+};
--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -2,38 +2,80 @@ import { Container } from "components/Container";
 import { Heading } from "components/Heading";
 import { Menu } from "components/Menu";
 import { Footer } from "components/Footer";
+import { BannerSlider } from "components/BannerSlider";
+import { GameCardSlider } from "components/GameCardSlider";
+import { Highlight, HightlightProps } from "components/Highlight";
+
+import { BannerProps } from "components/Banner";
+import { GameCardProps } from "components/GameCard";
 
 import * as S from "./styles";
 
-export const HomeTemplate = () => {
+export type HomeTemplateProps = {
+	banners: BannerProps[];
+	newGames: GameCardProps[];
+	mostPopularHighlight: HightlightProps;
+	mostPopularGames: GameCardProps[];
+	upcomingGames: GameCardProps[];
+	upcomingHighlight: HightlightProps;
+	upcomingMoreGames: GameCardProps[];
+	freeGamesHighlight: HightlightProps;
+	freeGames: GameCardProps[];
+};
+
+export const HomeTemplate = ({
+	banners,
+	newGames,
+	mostPopularHighlight,
+	mostPopularGames,
+	upcomingGames,
+	upcomingHighlight,
+	upcomingMoreGames,
+	freeGamesHighlight,
+	freeGames,
+}: HomeTemplateProps) => {
 	return (
 		<S.Wrapper>
 			<Container>
 				<Menu />
+
+				<BannerSlider items={banners} />
 			</Container>
 
 			<Container>
 				<Heading lineLeft lineColor="secondary" color="black">
 					News
 				</Heading>
+
+				<GameCardSlider items={newGames} />
 			</Container>
 
 			<Container>
 				<Heading lineLeft lineColor="secondary">
 					Most popular
 				</Heading>
+
+				<Highlight {...mostPopularHighlight} />
+				<GameCardSlider items={mostPopularGames} arrowColor="white" />
 			</Container>
 
 			<Container>
 				<Heading lineLeft lineColor="secondary">
 					Upcoming
 				</Heading>
+
+				<GameCardSlider items={upcomingGames} arrowColor="white" />
+				<Highlight {...upcomingHighlight} />
+				<GameCardSlider items={upcomingMoreGames} arrowColor="white" />
 			</Container>
 
 			<Container>
 				<Heading lineLeft lineColor="secondary">
 					Free games
 				</Heading>
+
+				<Highlight {...freeGamesHighlight} />
+				<GameCardSlider items={freeGames} arrowColor="white" />
 			</Container>
 
 			<Container>
