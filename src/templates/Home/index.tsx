5d4a2eca5049503c099f19a990e67ad1419a5b52
Washington Campos <washingtoncampos9@gmail.com>
2022-02-04 16:27:58 -0300

    feat: add static components to HomeTemplate

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,9 +1,44 @@
+import { Container } from "components/Container";
+import { Heading } from "components/Heading";
+import { Menu } from "components/Menu";
+import { Footer } from "components/Footer";
+
 import * as S from "./styles";
 
 export const HomeTemplate = () => {
 	return (
 		<S.Wrapper>
-			<h1>Home</h1>
+			<Container>
+				<Menu />
+			</Container>
+
+			<Container>
+				<Heading lineLeft lineColor="secondary" color="black">
+					News
+				</Heading>
+			</Container>
+
+			<Container>
+				<Heading lineLeft lineColor="secondary">
+					Most popular
+				</Heading>
+			</Container>
+
+			<Container>
+				<Heading lineLeft lineColor="secondary">
+					Upcoming
+				</Heading>
+			</Container>
+
+			<Container>
+				<Heading lineLeft lineColor="secondary">
+					Free games
+				</Heading>
+			</Container>
+
+			<Container>
+				<Footer />
+			</Container>
 		</S.Wrapper>
 	);
 };
