40181033548a22c05ceea9a29b6e613bd309238b
Washington Campos <washingtoncampos9@gmail.com>
2022-02-04 17:06:36 -0300

    refactor: add GameCardSlider mock to a separate file

---
+++ src/components/GameCardSlider/mock.ts
@@ -0,0 +1,38 @@
+export const items = [
+	{
+		img: "https://source.unsplash.com/user/willianjusten/1042x580",
+		title: "Card 1",
+		subtitle: "Description of card 1",
+		price: "R$ 215,00",
+	},
+	{
+		img: "https://source.unsplash.com/user/willianjusten/1042x580",
+		title: "Card 2",
+		subtitle: "Description of card 2",
+		price: "R$ 215,00",
+	},
+	{
+		img: "https://source.unsplash.com/user/willianjusten/1042x580",
+		title: "Card 3",
+		subtitle: "Description of card 3",
+		price: "R$ 215,00",
+	},
+	{
+		img: "https://source.unsplash.com/user/willianjusten/1042x580",
+		title: "Card 4",
+		subtitle: "Description of card 4",
+		price: "R$ 215,00",
+	},
+	{
+		img: "https://source.unsplash.com/user/willianjusten/1042x580",
+		title: "Card 5",
+		subtitle: "Description of card 5",
+		price: "R$ 215,00",
+	},
+	{
+		img: "https://source.unsplash.com/user/willianjusten/1042x580",
+		title: "Card 6",
+		subtitle: "Description of card 6",
+		price: "R$ 215,00",
+	},
+];
--- src/components/GameCardSlider/test.tsx
+++ src/components/GameCardSlider/test.tsx
@@ -5,45 +5,7 @@ import { screen } from "@testing-library/react";
 import { renderWithTheme } from "utils/tests/helpers";
 
 import { GameCardSlider } from ".";
-
-const items = [
-	{
-		img: "https://source.unsplash.com/user/willianjusten/1042x580",
-		title: "Card 1",
-		subtitle: "Description of card 1",
-		price: "R$ 215,00",
-	},
-	{
-		img: "https://source.unsplash.com/user/willianjusten/1042x580",
-		title: "Card 2",
-		subtitle: "Description of card 2",
-		price: "R$ 215,00",
-	},
-	{
-		img: "https://source.unsplash.com/user/willianjusten/1042x580",
-		title: "Card 3",
-		subtitle: "Description of card 3",
-		price: "R$ 215,00",
-	},
-	{
-		img: "https://source.unsplash.com/user/willianjusten/1042x580",
-		title: "Card 4",
-		subtitle: "Description of card 4",
-		price: "R$ 215,00",
-	},
-	{
-		img: "https://source.unsplash.com/user/willianjusten/1042x580",
-		title: "Card 5",
-		subtitle: "Description of card 5",
-		price: "R$ 215,00",
-	},
-	{
-		img: "https://source.unsplash.com/user/willianjusten/1042x580",
-		title: "Card 6",
-		subtitle: "Description of card 6",
-		price: "R$ 215,00",
-	},
-];
+import { items } from "./mock";
 
 describe("<GameCardSlider />", () => {
 	it("should render GameCardSlider with 4 active items", () => {
