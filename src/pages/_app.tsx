37078e21475ebc606e62167f64ab979eddc49cd7
Washington Campos <washingtoncampos9@gmail.com>
2022-02-04 15:57:17 -0300

    chore: add logo image to favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyles from "styles/global";
@@ -7,6 +8,13 @@ import theme from "styles/theme";
 const App = ({ Component, pageProps }: AppProps) => {
 	return (
 		<ThemeProvider theme={theme}>
+			<Head>
+				<title>Won Games</title>
+				<meta name="description" content="The best Game Store in the world" />
+
+				<link rel="shortcut icon" href="/logo.png" />
+				<link rel="apple-touch-icon" href="/logo.png" />
+			</Head>
 			<GlobalStyles />
 			<Component {...pageProps} />
 		</ThemeProvider>
