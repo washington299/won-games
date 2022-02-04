7b134cc311c7145efa0f3b993d6418cd2cfb2aa8
Washington Campos <washingtoncampos9@gmail.com>
2022-02-04 16:28:27 -0300

    test: ensure static components renders correctly

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,11 +1,23 @@
-import { render } from "@testing-library/react";
+import { screen } from "@testing-library/react";
+
+import { renderWithTheme } from "utils/tests/helpers";
 
 import { HomeTemplate } from ".";
 
 describe("<HomeTemplate />", () => {
-	it("should render correctly", () => {
-		const { container } = render(<HomeTemplate />);
+	it("should render Menu and Footer correctly", () => {
+		renderWithTheme(<HomeTemplate />);
 
-		expect(container.firstChild).toMatchSnapshot();
+		expect(screen.getByLabelText(/Open menu/i)).toBeInTheDocument();
+		expect(screen.getByRole("heading", { name: /Contact/i })).toBeInTheDocument();
+	});
+
+	it("should render sections correctly", () => {
+		renderWithTheme(<HomeTemplate />);
+
+		expect(screen.getByRole("heading", { name: /News/i })).toBeInTheDocument();
+		expect(screen.getByRole("heading", { name: /Most popular/i })).toBeInTheDocument();
+		expect(screen.getByRole("heading", { name: /Upcoming/i })).toBeInTheDocument();
+		expect(screen.getByRole("heading", { name: /Free games/i })).toBeInTheDocument();
 	});
 });
