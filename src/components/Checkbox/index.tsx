6878dff30ca0ee086535c27fb8eec773296680f0
Washington Campos <washingtoncampos9@gmail.com>
2022-02-06 15:45:11 -0300

    feat: add onCheck method to Checkbox

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,15 +1,32 @@
+import { InputHTMLAttributes, useState } from "react";
+
 import * as S from "./styles";
 
 export type CheckboxProps = {
 	label?: string;
 	labelFor?: string;
 	labelColor?: "white" | "black";
-};
+	onCheck?: (state: boolean) => void;
+} & InputHTMLAttributes<HTMLInputElement>;
 
-export const Checkbox = ({ label, labelFor = "", labelColor = "black" }: CheckboxProps) => {
+export const Checkbox = ({
+	label,
+	labelFor = "",
+	labelColor = "black",
+	onCheck,
+}: CheckboxProps) => {
+	const [checked, setChecked] = useState(false);
+
+	const onChange = () => {
+		const status = !checked;
+		setChecked(status);
+
+		!!onCheck && onCheck(status);
+	};
+
 	return (
 		<S.Wrapper>
-			<S.Input type="checkbox" id={labelFor} />
+			<S.Input type="checkbox" id={labelFor} onChange={onChange} />
 			{!!label && (
 				<S.Label htmlFor={labelFor} labelColor={labelColor}>
 					{label}
--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -5,6 +5,11 @@ import { Checkbox } from ".";
 export default {
 	title: "Checkbox",
 	component: Checkbox,
+	argTypes: {
+		onCheck: {
+			action: "checked",
+		},
+	},
 } as Meta;
 
 export const Default: Story = args => <Checkbox {...args} />;
