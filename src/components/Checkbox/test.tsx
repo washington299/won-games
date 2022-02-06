417db5d2b001058ee44588fa3ac450d2a4e019e2
Washington Campos <washingtoncampos9@gmail.com>
2022-02-06 15:00:20 -0300

    test: ensure Checkbox renders with correct label color

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -17,4 +17,23 @@ describe("<Checkbox />", () => {
 		expect(screen.getByLabelText(/My checkbox/i)).toBeInTheDocument();
 		expect(screen.getByText(/My checkbox/i)).toHaveAttribute("for", "my-checkbox");
 	});
+
+	it("should render Checkbox with correct label", () => {
+		renderWithTheme(<Checkbox label="My checkbox" labelFor="my-checkbox" />);
+
+		expect(screen.getByLabelText(/My checkbox/i)).toBeInTheDocument();
+		expect(screen.getByText(/My checkbox/i)).toHaveAttribute("for", "my-checkbox");
+	});
+
+	it("should render Checkbox with black label by default", () => {
+		renderWithTheme(<Checkbox label="My checkbox" labelFor="my-checkbox" />);
+
+		expect(screen.getByText(/My checkbox/i)).toHaveStyle({ color: "#030517" });
+	});
+
+	it("should render Checkbox with white label if prop is passed", () => {
+		renderWithTheme(<Checkbox label="My checkbox" labelFor="my-checkbox" labelColor="white" />);
+
+		expect(screen.getByText(/My checkbox/i)).toHaveStyle({ color: "#FAFAFA" });
+	});
 });
