b3b0beca4decb296bc085bbcae387b3a5be0071e
Washington Campos <washingtoncampos9@gmail.com>
2022-02-07 16:32:26 -0300

    feat: add value and onCheck props to Radio

--- src/components/Radio/index.tsx
+++ src/components/Radio/index.tsx
@@ -1,15 +1,32 @@
+import { InputHTMLAttributes } from "react";
+
 import * as S from "./styles";
+
+export type RadioValue = string | ReadonlyArray<string> | number;
 
 export type RadioProps = {
 	label?: string;
 	labelFor?: string;
 	labelColor?: "white" | "black";
-};
+	value?: RadioValue;
+	onCheck?: (value?: RadioValue) => void;
+} & InputHTMLAttributes<HTMLInputElement>;
 
-export const Radio = ({ label = "", labelFor = "", labelColor = "black" }: RadioProps) => {
+export const Radio = ({
+	label = "",
+	labelFor = "",
+	labelColor = "black",
+	value,
+	onCheck,
+	...props
+}: RadioProps) => {
+	const onChange = () => {
+		!!onCheck && onCheck(value);
+	};
+
 	return (
 		<S.Wrapper>
-			<S.Input type="radio" id={labelFor} />
+			<S.Input type="radio" id={labelFor} onChange={onChange} value={value} {...props} />
 			{!!label && (
 				<S.Label htmlFor={labelFor} labelColor={labelColor}>
 					{label}
--- src/components/Radio/stories.tsx
+++ src/components/Radio/stories.tsx
@@ -5,6 +5,9 @@ import { Radio, RadioProps } from ".";
 export default {
 	title: "Radio",
 	component: Radio,
+	argTypes: {
+		onCheck: { action: "checked" },
+	},
 } as Meta;
 
 export const Default: Story<RadioProps> = args => <Radio {...args} />;
