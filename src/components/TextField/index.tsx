fefeeeb6cda71284220bb9ad6dd7caebfa6c59f8
Washington Campos <washingtoncampos9@gmail.com>
2022-02-08 17:46:47 -0300

    feat: add onInput prop to TextField

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,15 +1,27 @@
+import React, { useState } from "react";
+
 import * as S from "./styles";
 
 export type TextFieldProps = {
 	label?: string;
 	labelFor?: string;
+	onInput?: (value: string) => void;
 };
 
-export const TextField = ({ label, labelFor = "" }: TextFieldProps) => {
+export const TextField = ({ label, labelFor = "", onInput }: TextFieldProps) => {
+	const [value, setValue] = useState("");
+
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const newValue = e.currentTarget.value;
+		setValue(newValue);
+
+		!!onInput && onInput(newValue);
+	};
+
 	return (
 		<S.Wrapper>
 			{!!label && <label htmlFor={labelFor}>{label}</label>}
-			<input id={labelFor} type="text" />
+			<input id={labelFor} type="text" value={value} onChange={onChange} />
 		</S.Wrapper>
 	);
 };
