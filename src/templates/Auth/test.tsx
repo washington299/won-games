4e6fb6bdc202e2df7e134b1633df077ef5b3fd95
Washington Campos <washingtoncampos9@gmail.com>
2022-02-10 16:17:34 -0300

    test: ensure Auth renders logo, titles and children correctly

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,11 +1,26 @@
-import { render } from "@testing-library/react";
+import { screen } from "@testing-library/react";
+
+import { renderWithTheme } from "utils/tests/helpers";
 
 import { Auth } from ".";
 
 describe("<Auth />", () => {
-	it("should render correctly", () => {
-		const { container } = render(<Auth />);
+	it("should render logo, titles and children correctly", () => {
+		renderWithTheme(
+			<Auth title="Title">
+				<input type="text" />
+			</Auth>,
+		);
 
-		expect(container.firstChild).toMatchSnapshot();
+		expect(screen.getAllByLabelText(/Won games/i)).toHaveLength(2);
+		expect(
+			screen.getByRole("heading", { name: /All your favorite games in one place/i }),
+		).toBeInTheDocument();
+		expect(
+			screen.getByRole("heading", { name: /Won is the best and most complete gaming platform./i }),
+		).toBeInTheDocument();
+		expect(screen.getByText(/Won Games 2020 © Todos os Direitos Reservados/i)).toBeInTheDocument();
+		expect(screen.getByRole("heading", { name: /Title/i })).toBeInTheDocument();
+		expect(screen.getByRole("textbox")).toBeInTheDocument();
 	});
 });
