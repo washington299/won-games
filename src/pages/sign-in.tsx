d2a5e9558ade897900d8bac0563a65d3e360fa66
Washington Campos <washingtoncampos9@gmail.com>
2022-02-12 04:05:45 -0300

    chore: add Forms in their pages

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,7 +1,13 @@
 import { Auth } from "templates/Auth";
 
+import { FormSignIn } from "components/FormSignIn";
+
 const SignIn = () => {
-	return <Auth title="Sign in">Children</Auth>;
+	return (
+		<Auth title="Sign in">
+			<FormSignIn />
+		</Auth>
+	);
 };
 
 export default SignIn;
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,7 +1,13 @@
 import { Auth } from "templates/Auth";
 
+import { FormSignUp } from "components/FormSignUp";
+
 const SignUp = () => {
-	return <Auth title="Sign Up">Children</Auth>;
+	return (
+		<Auth title="Sign Up">
+			<FormSignUp />
+		</Auth>
+	);
 };
 
 export default SignUp;
