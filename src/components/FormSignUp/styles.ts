4fba38649c84e658798d811283b806dbecb10647
Washington Campos <washingtoncampos9@gmail.com>
2022-02-12 03:28:09 -0300

    chore: add styles to FormSignUp

--- src/components/FormSignUp/styles.ts
+++ src/components/FormSignUp/styles.ts
@@ -1,5 +1,36 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { darken } from "polished";
 
-export const Wrapper = styled.main``;
+import * as TextFieldStyles from "components/TextField/styles";
+import * as ButtonStyles from "components/Button/styles";
 
-export const FormLink = styled.div``;
+export const Wrapper = styled.main`
+	${({ theme }) => css`
+		${TextFieldStyles.Wrapper} {
+			margin: ${theme.spacings.xxsmall} 0;
+		}
+
+		${ButtonStyles.Wrapper} {
+			margin: ${theme.spacings.xsmall} 0;
+		}
+	`}
+`;
+
+export const FormLink = styled.div`
+	${({ theme }) => css`
+		font-size: ${theme.font.sizes.small};
+		color: ${theme.colors.black};
+		text-align: center;
+
+		& > a {
+			text-decoration: none;
+			color: ${theme.colors.secondary};
+			border-bottom: 0.1rem solid ${theme.colors.secondary};
+
+			&:hover {
+				color: ${darken(0.1, theme.colors.secondary)};
+				border-bottom: 0.1rem solid ${darken(0.1, theme.colors.secondary)};
+			}
+		}
+	`}
+`;
