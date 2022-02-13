240904e146cb5f1b05b3d9fe241e07142a0ec577
Washington Campos <washingtoncampos9@gmail.com>
2022-02-13 16:24:22 -0300

    chore: remove pages folder from test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	testEnvironment: "jsdom",
 	testPathIgnorePatterns: ["/node_modules/", "/.next/"],
 	collectCoverage: true,
-	collectCoverageFrom: ["src/**/*.ts(x)", "!src/pages/", "!src/**/stories.tsx"],
+	collectCoverageFrom: ["src/**/*.ts(x)", "!src/pages/**/*.tsx", "!src/**/stories.tsx"],
 	setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
 	modulePaths: ["<rootDir>/src", "<rootDir>/.jest"],
 };
