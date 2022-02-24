ec737295261529e1e4cc8f2de30611ca057207e0
Washington Campos <washingtoncampos9@gmail.com>
2022-02-23 21:34:28 -0300

    test: ensure GameDetails renders platforms icons correctly

--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -9,7 +9,7 @@ const props: GameDetailsProps = {
 };
 
 describe("<GameDetails />", () => {
-	it("should render Headings correctly", () => {
+	it("Should render Headings correctly", () => {
 		renderWithTheme(<GameDetails {...props} />);
 
 		expect(screen.getByRole("heading", { name: /Developer/i })).toBeInTheDocument();
@@ -18,5 +18,13 @@ describe("<GameDetails />", () => {
 		expect(screen.getByRole("heading", { name: /publisher/i })).toBeInTheDocument();
 		expect(screen.getByRole("heading", { name: /Rating/i })).toBeInTheDocument();
 		expect(screen.getByRole("heading", { name: /Genres/i })).toBeInTheDocument();
+	});
+
+	it("Should render platforms icons correctly", () => {
+		renderWithTheme(<GameDetails {...props} />);
+
+		expect(screen.getByRole("img", { name: /windows/i })).toBeInTheDocument();
+		expect(screen.getByRole("img", { name: /linux/i })).toBeInTheDocument();
+		expect(screen.getByRole("img", { name: /mac/i })).toBeInTheDocument();
 	});
 });
