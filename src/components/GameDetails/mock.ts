f174fd3c46fb2d54e6f1e36df71fbc4496ad63b0
Washington Campos <washingtoncampos9@gmail.com>
2022-02-23 22:24:51 -0300

    chore: create mock GameDetails

---
+++ src/components/GameDetails/mock.ts
@@ -0,0 +1,8 @@
+export default {
+	developer: "Gearbox Software",
+	releaseDate: "2020-11-21T23:00:00",
+	platforms: ["windows", "linux", "mac"],
+	publisher: "2K",
+	rating: "BR0",
+	genres: ["Action", "Adventure"],
+};
