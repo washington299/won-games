8bfaf6430147e7dec3127b4939b914231f803498
Washington Campos <washingtoncampos9@gmail.com>
2022-02-24 14:51:18 -0300

    chore: create TextContent mock

---
+++ src/components/TextContent/mock.ts
@@ -0,0 +1,41 @@
+export default {
+	title: "Description",
+	content: `
+		<h1>Heading</h1>
+		<h2>Heading</h2>
+		<h3>Heading</h3>
+		<h4>Heading</h4>
+		<h5>Heading</h5>
+		<h6>Heading</h6>
+
+		<img src="/img/games/cyberpunk-1.jpg" alt="" />
+
+		<p>Reprehenderit cupidatat officia consequat irure cupidatat sint. Labore dolore proident est magna voluptate. Ut laborum eu fugiat eu veniam dolore et reprehenderit. Reprehenderit aliquip ipsum fugiat ut labore mollit elit. Sint laborum consequat enim commodo proident consequat sint deserunt fugiat sunt.</p>
+
+		<p>Reprehenderit cupidatat officia consequat irure cupidatat sint. Labore dolore proident est magna voluptate. Ut laborum eu fugiat eu veniam dolore et reprehenderit. Reprehenderit aliquip ipsum fugiat ut labore mollit elit. Sint laborum consequat enim commodo proident consequat sint deserunt fugiat sunt.</p>
+
+		<img src="/img/games/cyberpunk-2.jpg" alt="" />
+
+		<ul>
+			<li>Lorem Ipsum</li>
+			<li>Lorem Ipsum</li>
+			<li>Lorem Ipsum</li>
+			<li>Lorem Ipsum</li>
+			<li>Lorem Ipsum</li>
+		</ul>
+
+		<hr />
+
+		<ol>
+			<li>Lorem Ipsum</li>
+			<li>Lorem Ipsum</li>
+			<li>Lorem Ipsum</li>
+			<li>Lorem Ipsum</li>
+			<li>Lorem Ipsum</li>
+		</ol>
+
+		<p>
+			A <a href="#">Link inside the text</a> with Excepteur irure labore et sunt. Occaecat tempor cupidatat aute est. Pariatur commodo ipsum nulla cillum ullamco cillum ad. Aute sit veniam consequat velit do laborum. Irure deserunt adipisicing labore laboris aliqua ea ex minim ullamco.
+		</p>
+	`,
+};
