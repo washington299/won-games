cb1a9cd88ae6a0af6d3765e2df35c92393226857
Washington Campos <washingtoncampos9@gmail.com>
2022-02-25 15:33:12 -0300

    feat: create Gallery component and styles

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,62 @@
+import { Settings } from "react-slick";
+import {
+	ArrowBackIos as ArrowLeft,
+	ArrowForwardIos as ArrowRight,
+} from "@styled-icons/material-outlined";
+
+import { Slider } from "components/Slider";
+
+import * as S from "./styles";
+
+const settings: Settings = {
+	arrows: true,
+	slidesToShow: 4,
+	infinite: false,
+	lazyLoad: "ondemand",
+	prevArrow: <ArrowLeft aria-label="previous image" />,
+	nextArrow: <ArrowRight aria-label="next image" />,
+	responsive: [
+		{
+			breakpoint: 1375,
+			settings: {
+				arrows: false,
+				slidesToShow: 3.2,
+			},
+		},
+		{
+			breakpoint: 1024,
+			settings: {
+				arrows: false,
+				slidesToShow: 2.2,
+			},
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				slidesToShow: 1.2,
+			},
+		},
+	],
+};
+
+type GalleryImageProps = {
+	src: string;
+	alt: string;
+};
+
+export type GalleryProps = {
+	items: GalleryImageProps[];
+};
+
+export const Gallery = ({ items }: GalleryProps) => {
+	return (
+		<S.Wrapper>
+			<Slider settings={settings}>
+				{items.map(item => (
+					<img key={item.src} role="button" src={item.src} alt={item.alt} />
+				))}
+			</Slider>
+		</S.Wrapper>
+	);
+};
---
+++ src/components/Gallery/styles.ts
@@ -0,0 +1,61 @@
+import styled, { css } from "styled-components";
+import media from "styled-media-query";
+
+export const Wrapper = styled.div`
+	${({ theme }) => css`
+		${media.lessThan("huge")`
+			overflow-x: hidden;
+		`}
+
+		.slick-track,
+		.slick-list {
+			display: flex;
+		}
+
+		.slick-slide > div {
+			flex: 1 0 auto;
+			height: 100%;
+			margin: 0 ${theme.spacings.xxsmall};
+		}
+
+		.slick-list {
+			margin: 0 -${theme.spacings.xxsmall};
+		}
+
+		${media.greaterThan("large")`
+			.slick-slide > div {
+				margin: 0 ${theme.spacings.xsmall};
+			}
+
+			.slick-list {
+				margin: 0 -${theme.spacings.xsmall};
+			}
+		`}
+
+		.slick-prev,
+		.slick-next {
+			width: 2.5rem;
+			height: 2.5rem;
+			display: block;
+			position: absolute;
+			top: 50%;
+			transform: translate(0, -50%);
+			padding: 0;
+			color: ${theme.colors.white};
+			cursor: pointer;
+		}
+
+		.slick-prev {
+			left: -${theme.spacings.xxlarge};
+		}
+
+		.slick-next {
+			right: -${theme.spacings.xxlarge};
+		}
+
+		.slick-prev.slick-disabled,
+		.slick-next.slick-disabled {
+			visibility: hidden;
+		}
+	`}
+`;
